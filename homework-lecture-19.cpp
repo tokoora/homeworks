f09613f5592607d19fd8cc63ab7fdc6ce4fcd86f
T Oragvelidze <tokooragvelidze16@gmail.com>
2022-11-01 11:29:16 +0400

    Final

---
+++ homework-lecture-19.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char symbol='T';
+    int num=100;
+    float x=10.10;
+    double y=50.23;
+    int ar[5]={5, 10, 15, 20, 30};
+    int arSum=ar[0]+ar[1]+ar[2]+ar[3]+ar[4];
+    double arAvg=arSum/5;
+    cout<<arSum<<endl;
+    cout<<arAvg;
+}
